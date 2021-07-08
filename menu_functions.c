4dde61a4e1efea846baee05c86a9cf53c3758924
E H Murdoch <ehmurdoch47@protonmail.com>
2021-07-08 19:29:41 +0100

    finished count function; having issues with checking whether name exists

--- menu_functions.c
+++ menu_functions.c
@@ -6,9 +6,12 @@
 #include <stdlib.h>
 
 #include "species.h"
+#include "generic.h"
 
 void inputSpeciesName ( struct Species* species_data, char* input_buffer );
 int checkIfNameExists ( char* name, struct Species* species_data );
+
+void inputSpeciesCount ( char* input_buffer );
 
 void displaySpeciesDataAsTable ()
 {
@@ -23,30 +26,63 @@ void displaySpeciesDataAsBarChart ()
 void inputSpeciesData ( struct Species* species_data )
 {
     char input_buffer [ INPUT_LENGTH_LIMIT ];
+    
+    char name [ INPUT_LENGTH_LIMIT ];
+    int count;
+
+    struct Species *ptr;
+
+    int index = 0;
+    
+    ptr = species_data;
 
     puts ( "INPUT SPECIES DATA:" );
 
     puts ( "Inputting species data. Leave either field blank to stop." );
     puts ( "" );
 
+    // input species name (pass pointer to species array so we can check if
+    // a name already exists).
+    // 2021-06-08: for some reason this functionality isn't working
     inputSpeciesName ( species_data, input_buffer );
 
     // input until user leaves the input blank
-    while ( strcmp ( input_buffer, "" ) != 0 )
+    while ( strcmp ( input_buffer, "" ) != 0 && index < 16 )
     {
         // input species count
+        inputSpeciesCount ( input_buffer );
 
         // this if statement is needed since we are checking *both* fields
         // for a blank input.
         if ( strcmp ( input_buffer, "" ) != 0 )
         {
             // convert count to int
+            count = atoi ( input_buffer );
 
-            // build a record and add it to the array
+            // add the information to the current record
+            strcpy ( ptr -> name, name );
+            ptr -> count = count;
 
-            puts ( "" );
+            index ++;
+            ptr ++;
 
-            inputSpeciesName ( species_data, input_buffer );
+            if ( index < 16 )
+            {
+                puts ( "" );
+
+                inputSpeciesName ( species_data, input_buffer );
+            }
+            else
+            {
+                // from testing we see that pointer is not NULL. Don't know
+                // why though.
+                if ( ptr == NULL )
+                {
+                    puts ( "pointer is NULL." );
+                }
+                // future: reallocate memory for a larger array.
+                puts ( "Limit of 16 records reached." );
+            }
         }
     }
 
@@ -130,6 +166,35 @@ int checkIfNameExists ( char* name, struct Species* species_data )
     }
 
     return 0;
+}
+
+void inputSpeciesCount ( char* input_buffer )
+{
+    printf ( "Enter specimen count: " );
+
+    fgets ( input_buffer, INPUT_LENGTH_LIMIT, stdin );
+
+    input_buffer [ strcspn ( input_buffer, "\n" ) ] = 0;
+
+    while (
+        (
+            stringIsInteger ( input_buffer ) == 0 ||
+            // by limiting the user input to 9 or fewer characters, we ensure that
+            // we are always below the integer limit (2 147 483 547), meaning there
+            // will never be an integer overflow.
+            strlen ( input_buffer ) > 9
+        ) &&
+        strcmp ( input_buffer, "" ) != 0
+    )
+    {
+        puts ( "Invalid count." );
+
+        printf ( "Enter specimen count: " );
+
+        fgets ( input_buffer, INPUT_LENGTH_LIMIT, stdin );
+
+        input_buffer [ strcspn ( input_buffer, "\n" ) ] = 0;
+    }
 }
 
 void pickAndEditRecord ()
